ebb065cd1249ebe26a9dadd96a55f4284fa7c3c1
Anurag <anuragsingh8434845379@gmail.com>
2025-12-15 12:18:57 +0530

    Navbar

--- src/41ReactRouter.jsx
+++ src/41ReactRouter.jsx
@@ -1,11 +1,10 @@
-import {BrowserRouter,Routes,Route,Link} from 'react-router'
+import {BrowserRouter,Routes,Route} from 'react-router'
+import Navbar from './42NavBar'
 export default function ReactRouter(){
     return(
         <>
         <BrowserRouter>
-        <Link to='/'><h3>HOme</h3></Link>
-        <Link to='/about'><h3>About</h3></Link>
-        <Link to='/main'><h3>Main</h3></Link>
+        <Navbar/>
         <Routes>
             <Route path='/' element={<h2>Home</h2>}/>
             <Route path='/about' element={<h2>About</h2>}/>
---
+++ src/42NavBar.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router"
+import './42NavBar.css'
+export default function Navbar() {
+    return (
+        <div className="container">
+            <div className="logo">
+                <h3>Logo</h3>
+            </div>
+            <div className="linksContainer">
+                <Link className="link" to='/'><h3>HOme</h3></Link>
+                <Link className="link" to='/about'><h3>About</h3></Link>
+                <Link className="link" to='/main'><h3>Main</h3></Link>
+            </div>
+        </div>
+    )
+}
