815d82f3dcada4f309487848c3e90499871c459d
Anurag <anuragsingh8434845379@gmail.com>
2025-12-15 12:40:41 +0530

    404

--- src/41ReactRouter.jsx
+++ src/41ReactRouter.jsx
@@ -1,4 +1,5 @@
-import {BrowserRouter,Routes,Route} from 'react-router'
+import {BrowserRouter,Routes,Route, Navigate} from 'react-router'
+import PageNotFound from './43PageNotFound'
 import Navbar from './42NavBar'
 export default function ReactRouter(){
     return(
@@ -9,6 +10,10 @@ export default function ReactRouter(){
             <Route path='/' element={<h2>Home</h2>}/>
             <Route path='/about' element={<h2>About</h2>}/>
             <Route path='/main' element={<h2>Main</h2>}/>
+            <Route path='/*' element={<PageNotFound/>} />
+
+            {/* Auto redirect to "./" if 404 issue found*/}
+            {/* <Route path='/*' element={<Navigate to="./" /> }/> */}
         </Routes>
         </BrowserRouter>
         </>
