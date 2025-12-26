dc6c34c59b6e20d37ddc502ac64ea1c2842c4fe3
Anurag <anuragsingh8434845379@gmail.com>
2025-12-26 14:06:55 +0530

    put methos

--- src/47EditUser.jsx
+++ src/47EditUser.jsx
@@ -2,78 +2,81 @@ import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router";
 
 export default function EditUser() {
-  const [name, setName] = useState("");
-  const [classs, setClasss] = useState("");
-  const [age, setAge] = useState("");
+    const [name, setName] = useState("");
+    const [classs, setClasss] = useState("");
+    const [age, setAge] = useState("");
 
-  const { id } = useParams();
-  const navigate = useNavigate();
+    const { id } = useParams();
+    const navigate = useNavigate();
 
-  const getUserData = async () => {
-    const url = "http://localhost:3000/user/" + id;
-    let response = await fetch(url);
-    
-    response = await response.json();
-    console.log(response);
+    const getUserData = async () => {
+        const url = "http://localhost:3000/user/" + id;
+        let response = await fetch(url);
 
-    setName(response.name);
-    setClasss(response.classs);
-    setAge(response.age);
-  };
+        response = await response.json();
+        console.log(response);
 
-  useEffect(() => {
-    (
-        async ()=>{
-            await getUserData()
+        setName(response.name);
+        setClasss(response.classs);
+        setAge(response.age);
+    };
+
+    useEffect(() => {
+        (
+            async () => {
+                await getUserData();
+            }
+        )();
+    }, []);
+
+    const updateUser = async (name, age, classs) => {
+        console.log(name, age, classs);
+        const url = "http://localhost:3000/user/" + id;
+        let resposne = await fetch(url, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ name, classs, age })
+        });
+        resposne = await resposne.json();
+        if (resposne) {
+            alert("user Upadted");
+            navigate('/');
         }
-    )();
-  }, [id]);
 
-  const updateUser = async(name,age,classs)=>{
-    console.log(name,age,classs);
-    const url = "http://localhost:3000/user/" + id;
-    let resposne = await fetch (url,{
-        method: "put",
-        body : JSON.stringify({name,classs,age})
-    });
-    resposne = resposne.json();
-    if (resposne){
-        alert ("user Upadted");
-        navigate('/');
+
     }
 
-    
-  }
+    return (
+        <div style={{ textAlign: "center" }}>
+            <h2>Edit User Details</h2>
 
-  return (
-    <div style={{ textAlign: "center" }}>
-      <h2>Edit User Details</h2>
+            <input
+                type="text"
+                placeholder="User Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+            />
+            <br /><br />
 
-      <input
-        type="text"
-        placeholder="User Name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <br /><br />
+            <input
+                type="text"
+                placeholder="User Class"
+                value={classs}
+                onChange={(e) => setClasss(e.target.value)}
+            />
+            <br /><br />
 
-      <input
-        type="text"
-        placeholder="User Class"
-        value={classs}
-        onChange={(e) => setClasss(e.target.value)}
-      />
-      <br /><br />
+            <input
+                type="text"
+                placeholder="User Age"
+                value={age}
+                onChange={(e) => setAge(e.target.value)}
+            />
+            <br /><br />
 
-      <input
-        type="text"
-        placeholder="User Age"
-        value={age}
-        onChange={(e) => setAge(e.target.value)}
-      />
-      <br /><br />
-
-      <button onClick={()=>updateUser(name,age,classs)}>Update</button>
-    </div>
-  );
+            <button onClick={() => updateUser(name, age, classs)}>Update</button>
+        </div>
+    );
 }
