4cb5f157f6722a56991bc1920f79af925ce005c0
Anurag <anuragsingh8434845379@gmail.com>
2025-12-30 20:34:16 +0530

    UseAPIin react

--- src/53UseAPI.jsx
+++ src/53UseAPI.jsx
@@ -1,14 +1,26 @@
+import { Suspense,use } from "react";
+
+const fetchData=()=>fetch('https://dummyjson.com/users').then((response)=>response.json());
+const userResource= fetchData();
 export default function UseApi(){
     return(
         <>
-        <Userlist/>
+        <h2>Use Api in React</h2>
+        <Suspense fallback={<p>Loading..</p>}>
+        <Userlist userResource={userResource}/>
+        </Suspense>
         </>
     )
 }
-const Userlist =()=>{
+const Userlist =({userResource})=>{
+    const userdata = use(userResource);
     return(
         <>
         <h3>Hello Users</h3>
+        {userdata?.users?.map((user)=>(
+            <h3>{user.firstName}</h3>
+        ))
+        }
         </>
     )
 }
