ce0edcd9186e9e3fe93022981dbe83e0bd7567dc
Anurag <anuragsingh8434845379@gmail.com>
2026-01-04 19:58:00 +0530

    rd

--- src/56Redux/slice.jsx
+++ src/56Redux/slice.jsx
@@ -11,6 +11,10 @@ const addToCart= createSlice({
         addItem:(state,action)=>{
             state.items.push(action.payload);
         }
+        // },
+        // removeItem:(state,action)=>{
+        //     state.items= state.items.filter(item=>item.id !== action.payload)
+        // }
     }
 
 })
